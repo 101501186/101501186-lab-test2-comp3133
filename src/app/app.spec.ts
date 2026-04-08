0b82835b41d07fe71a8bf108fdbca8d15c9e7129
101501186 <101501186@georgebrown.ca>
2026-04-08 11:45:29 -0500

    Cleanup

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 101501186-lab-test2-comp3133');
+    expect(compiled.querySelector('h1')?.textContent).toContain('The Wizarding World');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 
 import { Characterdetails } from './components/characterdetails/characterdetails';
 import { Characterfilter } from './components/characterfilter/characterfilter';
@@ -9,12 +8,11 @@ import { Characterlist } from './components/characterlist/characterlist';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule, FormsModule, Characterfilter, Characterlist, Characterdetails],
+  imports: [HttpClientModule, FormsModule, Characterfilter, Characterlist, Characterdetails],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('101501186-lab-test2-comp3133');
   protected readonly selectedCharacterId = signal<string | null>(null);
   protected readonly selectedHouse = signal('all');
 
